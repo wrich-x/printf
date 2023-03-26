f9080b08b48396541342c0f8ed38bc4a4593efc5
wrich-x <wrichx.online@gmail.com>
2023-03-26 03:35:43 -0700

    Task 0 edit 1

---
+++ functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf - a function that produces output
+ * @format: format in function
+ * Return: printed characters
+ */
+
+int _printf(const char *format, ...)
+{
+}
