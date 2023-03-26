3d55bb4ac5241b1dec3b598078786c07780f81ee
druscillah <122983823+druscillah@users.noreply.github.com>
2023-03-26 23:20:22 +0300

    Update printf.c
    the function prints the formatted output to the standard output stream that is the console and returns the number of chars that were printed.  The format string can contain zero or more directives, which begin with a % character and specify how to format the corresponding argument.

--- printf.c
+++ printf.c
@@ -4,7 +4,7 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format.
+ * @format: format to be listed.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -64,4 +64,3 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
-
