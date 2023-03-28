ac317bbb83352000863a1899ad1b43842d93e605
druscillah <122983823+druscillah@users.noreply.github.com>
2023-03-28 12:28:27 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -10,15 +10,15 @@ int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
-
+	else
 	return (0);
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * append_hexa_code - Append ascii in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
+ * @ascii_code: ASCII CODE.
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
